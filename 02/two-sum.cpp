52a22b8538344dd2178627a390b946d8665b856a
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-04-06 23:55:29 +0530

    Update two-sum.cpp

--- 02/two-sum.cpp
+++ 02/two-sum.cpp
@@ -16,3 +16,17 @@ public:
     }
 };
 
+//Better (Space: O(n), Time: O(nlogn))
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        map<int, int>mp;
+        for(int i=0;i<n;i++){
+            auto it=mp.find(target-nums[i]);
+            if(it!=mp.end()) return {i, it->second};
+            else mp.insert(make_pair(nums[i], i));
+        }
+        return {-1,-1};
+    }
+};
