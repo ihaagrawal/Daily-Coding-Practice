d55ee92c844e5b57fab8ffef4cf95f7679ec3929
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-04-11 12:40:07 +0530

    Update 74. Search a 2D Matrix.cpp

--- 06/74. Search a 2D Matrix.cpp
+++ 06/74. Search a 2D Matrix.cpp
@@ -15,7 +15,8 @@ public:
     }
 };
 
-//Better (Space: O(1), Time: O(nlog(m*n))
+//Better (Space: O(1), Time: O(n))
+//Time: O(n) + O(logm) [The binary search will not happen more than once, it will only happen for 1 single row i.e. the one which contains the target]
 class Solution {
 public:
 
@@ -32,8 +33,31 @@ public:
 
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         for(int i=0;i<matrix.size();i++){
-            if(binary(matrix[i], target))
+            if(matrix[i][0]==target || matrix[i][matrix[0].size()-1]==target)
                 return true;
+            if(matrix[i][0]<target && matrix[i][matrix[0].size()-1]>target){
+                if(binary(matrix[i], target))
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+//Using binary search
+//Optimal (Space: O(1), Time: O(log(m*n))
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size(), n=matrix[0].size();
+        if(n==0 || m==0) return false;
+        int low=0, high=m*n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int midVal = matrix[mid / n][mid % n];
+            if(midVal==target) return true;
+            else if(midVal>target) high=mid-1;
+            else low=mid+1;
         }
         return false;
     }
