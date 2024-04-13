fd0cafec5ea66b69099e289e210004fb2629fe89
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-04-13 19:46:35 +0530

    Update 18. 4Sum.cpp

--- 08/18. 4Sum.cpp
+++ 08/18. 4Sum.cpp
@@ -36,3 +36,62 @@ public:
         return result;
     }
 };
+
+
+//Better (Space: O(n), Time: O(n^3log(m)))
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        set<vector<int>> st;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                set<long long> temp;
+                for(int k=j+1;k<n;k++){
+                    long long sum = nums[i]+nums[j];
+                    sum+=nums[k];
+                    long long fourth=target-sum;
+                    if(temp.find(fourth)!=temp.end()){
+                        vector<int> temp = {nums[i], nums[j], nums[k], (int)fourth};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
+                    }
+                    temp.insert(nums[k]);
+                }
+            }
+        }
+        vector<vector<int>> result(st.begin(), st.end());
+        return result;
+    }
+};
+
+//Optimal (Space: O(1), Time: O(n^3))
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n;j++){
+                if(j!=i+1 && nums[j]==nums[j-1]) continue;
+                int k=j+1, l=n-1;
+                while(k<l){
+                    long long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if(sum==target){
+                        result.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        k++, l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
+                    }
+                    else if(sum<target) k++;
+                    else l--;
+                }
+            }
+        }
+        return result;
+    }
+};
