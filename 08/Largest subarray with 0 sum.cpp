975bb50b87b2617605f9cdc4c9d52efb17befbfc
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-04-15 01:27:44 +0530

    Update Largest subarray with 0 sum.cpp

--- 08/Largest subarray with 0 sum.cpp
+++ 08/Largest subarray with 0 sum.cpp
@@ -21,3 +21,27 @@ public:
         return max_len;
     }
 };
+
+//Better (Space: O(n))
+//Time: Unordered Map -> Best: O(n), Worst: O(n^2)
+//      Ordered Map -> O(nlogn)
+
+class Solution{
+    public:
+    int maxLen(vector<int>&nums, int n)
+    {   
+        int k=0, sum=0, diff=0, len=0, max_len=0;
+        map<int, int> mp;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(sum==k) max_len=max(max_len, i+1);
+            diff=sum-k;
+            if(mp.find(diff)!=mp.end()){
+                len = i-mp[diff];
+                max_len=max(max_len, len);
+            }
+            if(mp.find(sum)==mp.end()) mp[sum]=i;
+        }
+        return max_len;
+    }
+};
