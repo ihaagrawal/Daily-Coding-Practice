d315aba0ee073812d9439390110582d5bb3e851c
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-04-17 11:23:43 +0530

    Create 206. Reverse Linked List.cpp

---
+++ 09/206. Reverse Linked List.cpp
@@ -0,0 +1,44 @@
+// 206. Reverse Linked List (EASY)
+// https://leetcode.com/problems/reverse-linked-list/description/
+
+//Brute (Space: O(n), Time: O(n))
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        stack<int>st;
+        ListNode* temp=head;
+        if (!head || !head->next)
+            return head;
+
+        while(temp){
+            st.push(temp->val);
+            temp=temp->next;
+        }
+        temp=head;
+        while(!st.empty()){
+            temp->val=st.top();
+            st.pop();
+            temp=temp->next;
+        }
+        return head;
+    }
+};
+
+//Better (Space: O(1), Time: O(n))
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* nextnode;
+        ListNode* current=head;
+        ListNode* prev=nullptr;
+
+        while(current){
+            nextnode=current->next;
+            current->next=prev;
+            prev=current;
+            current=nextnode;
+        }
+        head=prev;
+        return head;
+    }
+};
