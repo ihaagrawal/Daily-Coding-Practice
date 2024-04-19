6c14d85e45f4998dd5df75de1a38625eabf3ffc1
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-04-19 21:53:22 +0530

    Update 23. Merge k Sorted Lists.cpp

--- Linked List/23. Merge k Sorted Lists.cpp
+++ Linked List/23. Merge k Sorted Lists.cpp
@@ -25,3 +25,88 @@ public:
         return dummy->next;
     }
 };
+
+//Space Complexity is better, time complexity is worse
+//Better (Space: O(1), Time: O(n^3))
+class Solution {
+public:
+
+    ListNode* merge(ListNode* list1, ListNode* list2){
+        ListNode* dummy = new ListNode(-1);
+        dummy->next=nullptr;
+        ListNode* temp=dummy;
+
+        while(list1 && list2){
+            if(list1->val<=list2->val){
+                temp->next=list1;
+                list1=list1->next;
+            }
+            else{
+                temp->next=list2;
+                list2=list2->next;
+            }
+            temp=temp->next;
+        }
+
+        while(list1){
+            temp->next=list1;
+            list1=list1->next;
+            temp=temp->next;
+        }
+
+        while(list2){
+            temp->next=list2;
+            list2=list2->next;
+            temp=temp->next;
+        }
+
+        return dummy->next;
+
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int k=lists.size();
+        if(k==1) return lists[0];
+        while(k > 1){
+            lists[0] = merge(lists[0], lists[1]);
+            lists.erase(lists.begin() + 1);
+            k--;
+        }
+        return k == 0 ? nullptr : lists[0];
+    }
+};
+
+
+//Optimal (Space: O(k) , Time: O(klogk) + O(n*k*logk))
+class Solution {
+public:
+
+    struct compare{
+        bool operator()(const pair<int, ListNode*>& a, const pair<int, ListNode*>& b){
+            return a.first>b.first;
+        }
+    };
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+    priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, compare> pq;
+        int k=lists.size();
+        for(int i=0;i<k;i++){
+            if(lists[i]){
+                pq.push({lists[i]->val, lists[i]});
+            }
+        }
+
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            if(it.second->next) pq.push({it.second->next->val, it.second->next});
+            temp->next=it.second;
+            temp=temp->next;
+        }
+
+        return dummy->next;
+    }
+};
