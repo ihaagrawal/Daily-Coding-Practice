8d512a421a652b3261aa95cc8a4a2a0d7b77521e
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-04-19 23:37:33 +0530

    Update 26. Remove Duplicates from Sorted Array.cpp

--- Arrays/26. Remove Duplicates from Sorted Array.cpp
+++ Arrays/26. Remove Duplicates from Sorted Array.cpp
@@ -1,7 +1,7 @@
 // 26. Remove Duplicates from Sorted Array
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?source=submission-ac
 
-//Brute
+//Brute (Space: O(1), Time: O(n^2))
 
 class Solution {
 public:
@@ -19,8 +19,21 @@ public:
     }
 };
 
+//Better (Space: O(n), Time: O(2n))
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        set<int> st;
+        for(int i=0;i<n;i++) st.insert(nums[i]);
+        int i=0;
+        for(auto num:st) nums[i++]=num;
+        return st.size();
+    }
+};
 
-//Better
+
+//Optimal (Space: O(1), Time: O(n))
 
 class Solution {
 public:
