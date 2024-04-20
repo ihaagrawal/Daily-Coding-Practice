991d1322894bfecf9aef46ed8b8fed6f3be87db1
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-04-20 22:59:53 +0530

    Create BFS of graph.cpp

---
+++ Graphs/BFS of graph.cpp
@@ -0,0 +1,26 @@
+// BFS of graph (EASY)
+// https://www.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
+
+class Solution {
+  public:
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        vector<bool> visited(V, false);  //visited array to keep track if node is visited or not
+        vector<int> result;
+        queue<int> q;
+        q.push(0);
+        visited[0]=true;
+        
+        while(!q.empty()){
+            int ind=q.front();
+            q.pop();
+            result.push_back(ind);
+            for(auto j:adj[ind]){
+                if(!visited[j]){
+                    q.push(j);
+                    visited[j]=true;
+                }
+            }
+        }
+        return result;
+    }
+};
