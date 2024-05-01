9ba6113af6df76ff2ea7f274dbf31e41c34ac5ff
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-05-01 22:27:24 +0530

    Update Dijkstra's shortest path.cpp

--- Graphs/Shortest Path/Dijkstra's shortest path.cpp
+++ Graphs/Shortest Path/Dijkstra's shortest path.cpp
@@ -1,6 +1,8 @@
 // Dijkstra's shortest path (EASY)
 // https://www.naukri.com/code360/problems/dijkstra's-shortest-path_985358?leftPanelTabValue=PROBLEM
 
+
+//Using Priority Queue
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -32,6 +34,50 @@ vector<int> dijkstra(vector<vector<int>>& edges, int n, int m, int src) {
             }
         }
     }
+
+
+    //Using Set
+    #include <bits/stdc++.h>
+using namespace std;
+
+vector<int> dijkstra(vector<vector<int>>& edges, int n, int m, int src) {
+    set<pair<int, int>> st;
+
+    vector<pair<int, int>> adj[n];
+    for(auto edge:edges){
+            int v=edge[0], u=edge[1], w=edge[2];
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+        }
+
+    vector<int> dist(n, INT_MAX);
+    dist[src] = 0;
+    st.insert({0, src});
+
+    while (!st.empty()) {
+        auto it=st.begin();
+        int num = it->second;
+        int dis = it->first;
+        st.erase(it);
+
+        for (auto adjnode : adj[num]) {
+            int v = adjnode.first;
+            int w = adjnode.second;
+            if (dist[v] > dis + w) {
+                auto found = st.find({dist[v], v});
+                if(found != st.end())
+                    st.erase(found);
+                dist[v] = dis + w;
+                st.insert({dist[v], v});
+            }
+        }
+    }
+
+    return dist;
+}
+
+
+
 
     return dist;
 }
