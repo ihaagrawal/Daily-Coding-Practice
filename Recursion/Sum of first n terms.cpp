d119c71f971f4bcd5ca6e95ee33af3d11f0c975e
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-06-03 16:53:26 +0530

    Create Sum of first n terms.cpp

---
+++ Recursion/Sum of first n terms.cpp
@@ -0,0 +1,19 @@
+// Sum of first n terms (BASIC)
+// https://www.geeksforgeeks.org/problems/sum-of-first-n-terms5843/1
+
+class Solution {
+  public:
+  
+    void sumNums (long long& sum, long long n){
+        if(n==0) return;
+        
+        sum += n*n*n;
+        sumNums(sum, n-1);
+    }
+  
+    long long sumOfSeries(long long n) {
+        long long sum = 0;
+        sumNums(sum, n);
+        return sum;
+    }
+};
