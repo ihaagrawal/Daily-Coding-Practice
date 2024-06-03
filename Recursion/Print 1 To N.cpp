00ca3fe13b4fa12a44d8ee3ce7298392517055be
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-06-03 16:51:13 +0530

    Create Print 1 To N.cpp

---
+++ Recursion/Print 1 To N.cpp
@@ -0,0 +1,14 @@
+// Print 1 To N Without Loop (SCHOOL)
+// https://www.geeksforgeeks.org/problems/print-1-to-n-without-using-loops-1587115620/1
+
+class Solution{
+    public:
+    //Complete this function
+    void printNos(int N)
+    {
+        if(N==0) return;        
+        
+        printNos(N-1);
+        cout << N << " ";
+    }
+};
