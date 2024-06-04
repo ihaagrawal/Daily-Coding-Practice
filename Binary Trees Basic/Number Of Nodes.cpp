94d1b3904bbba97ab86a913d602aa6ce6d976db3
Iha Agrawal <ihaagrawal2004@gmail.com>
2024-06-04 11:54:01 +0530

    Committing local changes before reverting

--- Binary Trees Basic/Number Of Nodes.cpp
+++
@@ -1,8 +0,0 @@
-// Number Of Nodes
-// https://www.naukri.com/code360/problems/number-of-nodes_8162204?leftPanelTabValue=PROBLEM
-
-int numberOfNodes(int N){
-    return pow(2, N-1);
-}
-
-// For nth level a binary tree can have max 2^(n-1) nodes
