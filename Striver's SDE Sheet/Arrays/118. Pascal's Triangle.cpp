b04b84d3cd1954d941e97101c649eed56e366a69
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-06-05 16:34:06 +0530

    Update and rename Pascal's Triangle.cpp to 118. Pascal's Triangle.cpp

--- Striver's SDE Sheet/Arrays/118. Pascal's Triangle.cpp
+++ Striver's SDE Sheet/Arrays/118. Pascal's Triangle.cpp
@@ -66,6 +66,33 @@ public:
 // Pascal's Triangle (EASY)
 // https://leetcode.com/problems/pascals-triangle/description/
 
+//Brute
+//Time Complexity: O(numRows * numRows)
+//Space Complexity: O(1)
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        
+        if(numRows == 0) return result; 
+        result.push_back({1});
+
+        for(int i=1;i<numRows;i++){
+            vector<int> temp = {1};
+            vector<int> prev = result.back();
+            for(int j=0;j<i-1;j++){
+                int num = prev[j] + prev[j+1];
+                temp.push_back(num);
+            }
+            temp.push_back(1);
+            result.push_back(temp);
+        }
+
+        return result;
+    }
+};
+
+
 
 
 
