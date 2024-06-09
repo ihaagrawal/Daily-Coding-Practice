995a775b9e5789e434c6b607256004ca2268a937
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-06-09 18:11:05 +0530

    Create 2.  N2-Forest.cpp

---
+++ Striver A2Z DSA Sheet/Pattern/2.  N2-Forest.cpp
@@ -0,0 +1,10 @@
+// N/2-Forest (EASY)
+// https://www.naukri.com/code360/problems/n-2-forest_6570178
+
+void nForest(int n) {
+	for(int i=0;i<n;i++){
+		for(int j=0;j<=i;j++){
+			cout << "* ";
+		}cout << endl;
+	}
+}
