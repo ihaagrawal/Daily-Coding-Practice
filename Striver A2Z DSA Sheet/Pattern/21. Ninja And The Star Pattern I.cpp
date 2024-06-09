7ca4340300f97c1df28a941baa02806b179b56b4
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-06-09 18:25:04 +0530

    Create 21. Ninja And The Star Pattern I.cpp

---
+++ Striver A2Z DSA Sheet/Pattern/21. Ninja And The Star Pattern I.cpp
@@ -0,0 +1,16 @@
+// Ninja And The Star Pattern I (EASY)
+// https://www.naukri.com/code360/problems/ninja-and-the-star-pattern-i_6581920
+
+void getStarPattern(int n) {
+    for(int i=1;i<=n;i++){
+      if(i==1 || i==n){
+        for(int j=1;j<=n;j++) cout << "*";
+      }else{
+        for(int j=1;j<=n;j++){
+          if(j==1 || j==n) cout << "*";
+          else cout << " ";
+        }
+      }
+      cout << endl;
+    }
+}
