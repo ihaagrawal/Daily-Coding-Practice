381701294beb5cf55ed3dfc1c075c4d0b76f51df
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-06-09 18:12:20 +0530

    Create 4. Triangle.cpp

---
+++ Striver A2Z DSA Sheet/Pattern/4. Triangle.cpp
@@ -0,0 +1,10 @@
+// Triangle (EASY)
+// https://www.naukri.com/code360/problems/triangle_6573690
+
+void triangle(int n) {
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			cout << i << " ";
+		}cout << endl;
+	}
+}
