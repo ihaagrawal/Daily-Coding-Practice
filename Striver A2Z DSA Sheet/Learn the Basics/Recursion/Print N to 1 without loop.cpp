838807ee9780c945d395a57c63f8ed10037d0499
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-06-10 17:07:39 +0530

    Create Print N to 1 without loop.cpp

---
+++ Striver A2Z DSA Sheet/Learn the Basics/Recursion/Print N to 1 without loop.cpp
@@ -0,0 +1,11 @@
+// Print N to 1 without loop (EASY)
+// https://www.geeksforgeeks.org/problems/print-n-to-1-without-loop/1
+
+class Solution {
+  public:
+    void printNos(int n) {
+        if(n==0) return;
+        cout << n << " ";
+        printNos(n-1);
+    }
+};
