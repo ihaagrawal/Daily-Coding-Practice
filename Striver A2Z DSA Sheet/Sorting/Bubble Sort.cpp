31ad3afd24724441996c53c28c330d7ec3d7d9a2
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-06-16 22:26:03 +0530

    Create Bubble Sort.cpp

---
+++ Striver A2Z DSA Sheet/Sorting/Bubble Sort.cpp
@@ -0,0 +1,16 @@
+// Bubble Sort (EASY)
+// https://www.geeksforgeeks.org/problems/bubble-sort/1
+
+class Solution {
+  public:
+    // Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int arr[], int n) {
+       for(int i=0;i<n;i++){
+           for(int j=0;j<n-i-1;j++){
+               if(arr[j] > arr[j+1]){
+                   swap(arr[j], arr[j+1]);
+               }
+           }
+       }
+    }
+};
